100bdecb2da49529f5c550456931ce3c63c4766d
chrisic3 <chris_ic3@hotmail.com>
2025-12-05 18:58:24 +0000

    Refactor imgs Into public Folder and Fix Image Not Displaying

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
